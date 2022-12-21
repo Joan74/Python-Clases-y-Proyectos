c8b91d601a1524ab542dba0851b41143dd2b0753
Joan74 <joan151006@gmail.com>
2022-12-21 19:43:19 +0100

    new

--- Curso Python/12-Modulos/main.py
+++ Curso Python/12-Modulos/main.py
@@ -7,10 +7,10 @@ por internet, o crear nuestros modulos
 """
 
 # Importar modulo propio
-# import mimodulo
-# print(mimodulo.holaMundo("Juan Manuel")) # Se pude llamar asi
-# print(mimodulo.calculadora(3, 5, True))
-# from mimodulo import holaMundo # asi con menos codigo
+import mimodulo
+print(mimodulo.holaMundo("Juan Manuel")) # Se pude llamar asi
+print(mimodulo.calculadora(3, 5, True))
+from mimodulo import holaMundo # asi con menos codigo
 
 from mimodulo import * # Asi llamara a todo lo que haya en el modulo
 print(holaMundo("Juan Manuel"))
@@ -29,7 +29,17 @@ print(fecha_completa.day)
 fecha_personalizada = fecha_completa.strftime("%d/%m/%Y, ") # La fecha como tu quieres qque salga dia mes año
 print(fecha_personalizada)
 
+# Modulos matematicas
 
+import math
 
+print("Raiz cuadrada de 10: ", math.sqrt(10)) # raiz cuadradada de 10
+print("Numero Pi: ", float(math.pi))
+print("Redondear Baja: ", math.floor(7.78797))
+print("Redondear Alza : ", math.ceil(7.78797))
 
+# Modulo random
+import random
+
+print("Numero aletorio entre 15 y 67: ", random.randint(15, 67))
 
---
+++ Curso Python/13-Paquetes/main.py
@@ -0,0 +1,5 @@
+print("PROBANDO PAQUETES:")
+from mipaquete import pruebas, herramientas
+
+pruebas.probando()
+herramientas.nombreCompleto("Juan", "Barrio")
