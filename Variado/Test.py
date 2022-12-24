02e6f1e3974e9efbdf66b8e5219bf23e0e37fde1
Joan74 <joan151006@gmail.com>
2022-12-24 08:11:06 +0100

    Mejorado

--- Variado/Test.py
+++ Variado/Test.py
@@ -10,87 +10,88 @@ print("                         ------------------ TEST CIENCIAS VERDADERO O FAL
 print("                         ---------------------------------------------------------------------")
 
 print("\n")
-print("PULSA 1 PARA VERDADERO")
-print("PULSA 2 PARA FALSO")
+print("PULSA 1 VERDADERO")
+print("PULSA 2 FALSO")
 print()
-
-preguntas = ["Los electrones son mas pequeños que los atomos: ", "Toda la radioactidad es producida por el Hombre: ",
-             "Los antivioticos curan enfermedades causadas tanto por virus como por bacterias: ", "Los seres humanos provienen de especies animales anteriores: ",
-             "Los rayos laser funcionan mediante la concentracion de ondas de sonido: ", "Los continentes se han estado moviendo a lo largo de millones de años y continuarán hacíendolo en el futuro: ",
-             "El centro de la Tierra está muy caliente: ", "El Sol gira alrededor de la Tierra: ", "Los primeros humanos vivieron al mismo tiempo que los dinosaurios: ",
-             "El sol gira alrededor de la tierra (sí, esta es repetida): "]
+# PREGUNTAS
+preguntas = ["Los electrones son mas pequeños que los atomos ", "Toda la radioactidad es producida por el Hombre ",
+             "Los antivioticos curan enfermedades causadas tanto por virus como por bacterias ", "Los seres humanos provienen de especies animales anteriores ",
+             "Los rayos laser funcionan mediante la concentracion de ondas de sonido ", "Los continentes se han estado moviendo a lo largo de millones de años y continuarán hacíendolo en el futuro ",
+             "El centro de la Tierra está muy caliente ", "El Sol gira alrededor de la Tierra ", "Los primeros humanos vivieron al mismo tiempo que los dinosaurios ",
+             "El sol gira alrededor de los planetas"]
 
 for pregunta in preguntas:
     print(f"{preguntas.index(pregunta)+1}. {pregunta}")
 print()
+# RESPUESTAS
 opcion = int(input(f"\nRespuesta pregunta 1: "))
 if opcion == 1:
-    print("VERDADERO")
+    print("CORRECTO")
     aciertos += 1
 else:
-    print("FALSO")
+    print("INCORRECTO")
     fallos += 1
 opcion = int(input(f"Respuesta pregunta 2: "))
 if opcion == 2:
-    print("VERDADERO")
+    print("CORRECTO")
     aciertos += 1
 else:
-    print("FALSO")
+    print("INCORRECTO")
     fallos += 1
 opcion = int(input(f"Respuesta pregunta 3: "))
 if opcion == 2:
-    print("VERDADERO")
+    print("CORRECTO")
     aciertos += 1
 else:
-    print("FALSO")
+    print("INCORRECTO")
     fallos += 1
 opcion = int(input(f"Respuesta pregunta 4: "))
 if opcion == 1:
-    print("VERDADERO")
+    print("CORRECTO")
     aciertos += 1
 else:
-    print("FALSO")
+    print("INCORRECTO")
     fallos += 1
 opcion = int(input(f"Respuesta pregunta 5: "))
 if opcion == 2:
-    print("VERDADERO")
+    print("CORRECTO")
     aciertos += 1
 else:
-    print("FALSO")
+    print("INCORRECTO")
     fallos += 1
 opcion = int(input(f"Respuesta pregunta 6: "))
 if opcion == 1:
-    print("VERDADERO")
+    print("CORRECTO")
     aciertos += 1
 else:
-    print("FALSO")
+    print("INCORRECTO")
     fallos += 1
 opcion = int(input(f"Respuesta pregunta 7: "))
 if opcion == 1:
-    print("VERDADERO")
+    print("CORRECTO")
     aciertos += 1
 else:
-    print("FALSO")
+    print("INCORRECTO")
 opcion = int(input(f"Respuesta pregunta 8: "))
 if opcion == 2:
-    print("VERDADERO")
+    print("CORRECTO")
     aciertos += 1
 else:
-    print("FALSO")
+    print("INCORRECTO")
     fallos += 1
 opcion = int(input(f"Respuesta pregunta 9: "))
 if opcion == 2:
-    print("VERDADERO")
+    print("CORRECTO")
     aciertos += 1
 else:
-    print("FALSO")
+    print("INCORRECTO")
     fallos += 1
 opcion = int(input(f"Respuesta pregunta 10: "))
 if opcion == 2:
-    print("VERDADERO")
+    print("CORRECTO")
     aciertos += 1
 else:
-    print("FALSO")
+    print("INCORRECTO")
     fallos += 1
 print(f"\nPreguntas acertadas : {aciertos}")
 print(f"Preguntas falladas : {fallos} ")
