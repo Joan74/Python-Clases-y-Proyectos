313eba331c9abb480f55cebb8021ed6900b52b7e
Joan74 <joan151006@gmail.com>
2023-01-13 17:48:52 +0100

    ok

--- Variado/ejercicios.py
+++ Variado/ejercicios.py
@@ -1,24 +1,24 @@
 print("\n-----------------------------------------------------------------------------------------------------------------")
-
+numero = 0
 
 def calculadora():
-    numero1 = int(input("\nIntroduce numero 1: "))
-    numero2 = int(input("Introduce numero 2: "))
+    numero1 = int(input("\nIntroduce numero a multiplicar 1: "))
+    numero2 = int(input("Introduce numero a multiplicar 2: "))
     print(f"La Multiplicacion es {numero1*numero2}")
-    for base in range(numero1, numero2):
-        print(f"{base} X {base} = {base*base}")
-
+    
+    demanda()
 
 def demanda():
     n1 = int(input("\nIntroduce numero 1: "))
     n2 = int(input("Introduce numero 2: "))
     if n1 < n2:
-        for numero in range(n1 + 1, n2):
-            print(numero)
-            calculadora()
+        for numero in range(n1,n2):
+            print(numero) 
+            
     else:
-        print(f"\nEl numero 1 debe ser menor al dos")
-        demanda()
+        print(f"\nEl numero 1 debe ser menor al dos") 
+       
+    return demanda
 
+calculadora()
 
-demanda()
