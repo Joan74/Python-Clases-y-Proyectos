19b15167a27b9c7e194db77075e097512b4d5d3f
Joan74 <joan151006@gmail.com>
2023-02-13 11:21:17 +0100

    ok

--- Variado/laiapruebas.py
+++ Variado/laiapruebas.py
@@ -1,10 +1,3 @@
-import os
-os.system("clear")
-
-n1 = float(input("Introduce un numero"))
-n2 = float(input("Introduce segundo numero"))
-
-print(" La Suma es: " + str(n1+n2))
-print(" La Resta es: " + str(n1-n2))
-print(" La Multiplicacion es: " + str(n1*n2))
-print(" La Division es: " + str(n1 / n2))
+peliculas = ["X-MEN", "X-MEN2", "X-MEN3"]
+print(peliculas[-2])
+print(peliculas[2:])
